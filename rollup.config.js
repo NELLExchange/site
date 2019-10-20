2bb272e33ff0ddb3c96094c19e993e079cefa6cd
Damien <anamorphism@gmail.com>
2019-10-20 10:44:56 -0700

    Add basic boilerplate for the site
    Let's give Svelte and Bulma a try.

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/bundle.js'
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      css: css => {
+        css.write('public/bundle.css');
+      }
+    }),
+
+    postcss(),
+
+    resolve({
+      browser: true,
+      dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
+    }),
+    commonjs(),
+
+    !production && livereload('public'),
+
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
+};
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,5 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    Norwegian-English Language Learning Exchange
+  </div>
+</nav>
