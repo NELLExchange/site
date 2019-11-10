c9c8caaf247f9902a71194f9d667039951f30129
anamorphism <1200991+anamorphism@users.noreply.github.com>
2019-11-09 16:02:03 -0800

    Use Sapper
    Convert existing site to use Sapper.
    
    This is so we can use routing and other features.

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,106 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
 import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import sveltePreprocess from 'svelte-preprocess';
+import pkg from './package.json';
 
-const production = !process.env.ROLLUP_WATCH;
+const preprocess = sveltePreprocess();
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
 
 export default {
-  input: 'src/main.js',
-  output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'app',
-    file: 'public/bundle.js'
+  client: {
+    input: config.client.input(),
+    output: config.client.output(),
+    plugins: [
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+      svelte({
+        preprocess,
+        dev,
+        hydratable: true,
+        emitCss: true
+      }),
+      resolve({
+        browser: true,
+        dedupe
+      }),
+      commonjs(),
+
+      legacy && babel({
+        extensions: ['.js', '.mjs', '.html', '.svelte'],
+        runtimeHelpers: true,
+        exclude: ['node_modules/@babel/**'],
+        presets: [
+          ['@babel/preset-env', {
+            targets: '> 0.25%, not dead'
+          }]
+        ],
+        plugins: [
+          '@babel/plugin-syntax-dynamic-import',
+          ['@babel/plugin-transform-runtime', {
+            useESModules: true
+          }]
+        ]
+      }),
+
+      !dev && terser({
+        module: true
+      })
+    ],
+
+    onwarn,
   },
-  plugins: [
-    svelte({
-      dev: !production,
-      css: css => {
-        css.write('public/bundle.css');
-      }
-    }),
 
-    postcss(),
+  server: {
+    input: config.server.input(),
+    output: config.server.output(),
+    plugins: [
+      replace({
+        'process.browser': false,
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+      svelte({
+        preprocess,
+        generate: 'ssr',
+        dev
+      }),
+      resolve({
+        dedupe
+      }),
+      commonjs()
+    ],
+    external: Object.keys(pkg.dependencies).concat(
+      require('module').builtinModules || Object.keys(process.binding('natives'))
+    ),
 
-    resolve({
-      browser: true,
-      dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-    }),
-    commonjs(),
+    onwarn,
+  },
 
-    !production && livereload('public'),
+  serviceworker: {
+    input: config.serviceworker.input(),
+    output: config.serviceworker.output(),
+    plugins: [
+      resolve(),
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+      commonjs(),
+      !dev && terser()
+    ],
 
-    production && terser()
-  ],
-  watch: {
-    clearScreen: false
+    onwarn,
   }
 };
--- src/App.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import './app.scss';
-
-  import NavBar from './components/NavBar.svelte';
-</script>
-
-<NavBar/>
