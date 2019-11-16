48a53cbb79333cf1b61c743c0bba392d1e7217a2
anamorphism <1200991+anamorphism@users.noreply.github.com>
2019-11-16 10:51:44 -0800

    Flesh out dictionaries a bit more
    Add a few actual dictionaries with relevant information.
    
    Change layout to only look mildly terrible.

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -4,7 +4,7 @@
   }
 </script>
 
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar is-dark" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href=".">
       NELLE
--- src/routes/resources/dictionaries.svelte
+++ src/routes/resources/dictionaries.svelte
@@ -1,31 +1,67 @@
 <script>
   const dictionaries = [
     {
-      description: 'Some description.',
+      name: 'Universitet i Bergens Ordbok',
+      description: 'The University of Bergen\'s dictionary.',
       href: 'https://ordbok.uib.no/',
-      name: 'Some Name',
+      tags: {
+        bokmål: true,
+        nynorsk: true,
+        english: false,
+        examples: true,
+        inflections: true,
+      },
     },
     {
-      description: 'Some description 2.',
-      href: 'https://ordbok.uib.no/',
-      name: 'Some Name 2',
+      name: 'Norske Akademis Ordbok',
+      description: 'The Norwegian Academy\'s dictionary.',
+      href: 'https://www.naob.no/ordbok/',
+      tags: {
+        bokmål: true,
+        nynorsk: true,
+        english: false,
+        examples: true,
+        inflections: true,
+      },
+    },
+    {
+      name: 'bab.la',
+      description: 'bab.la\'s Norwegian-English dicationary.',
+      href: 'https://en.bab.la/dictionary/norwegian-english/',
+      tags: {
+        bokmål: true,
+        nynorsk: false,
+        english: true,
+        examples: true,
+        inflections: false,
+      },
     },
   ];
 </script>
 
+<h1 class="title">Dictionaries</h1>
 
-<div class="container">
-  <h1 class="title">Dictionaries</h1>
+{#each dictionaries as dict}
+  <section class="section">
+    <header>
+      <h2 class="title is-4">{dict.name}</h2>
+      <h3 class="subtitle is-5">
+        <a href={dict.href}>{dict.href}</a>
+      </h3>
 
-  {#each dictionaries as dict}
-    <section class="section">
-      <div class="container">
-        <h1 class="title">{dict.name}</h1>
-        <h2 class="subtitle">
-          <a href={dict.href}>{dict.href}</a>
-        </h2>
-        <p>{dict.description}</p>
+      <div class="field is-grouped is-grouped-multiline">
+        {#each Object.keys(dict.tags) as tag}
+          <div class="control">
+            <div class="tags has-addons">
+              <span class="tag is-dark">{tag}</span>
+              <span class="tag {dict.tags[tag] ? 'is-success' : 'is-danger'}">{dict.tags[tag]}</span>
+            </div>
+          </div>
+        {/each}
       </div>
-    </section>
-  {/each}
-</div>
+    </header>
+
+      <p>{dict.description}</p>
+
+  </section>
+{/each}
