557aca00cbd6932472fa527d34c009cff92e099b
anamorphism <1200991+anamorphism@users.noreply.github.com>
2020-01-30 16:41:07 -0800

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
+import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import svelte from 'rollup-plugin-svelte';
 import { terser } from 'rollup-plugin-terser';
