af358812e9e12d109a94d531c342619adcd998ca
anamorphism <1200991+anamorphism@users.noreply.github.com>
2020-03-29 11:51:45 -0700

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
 import svelte from 'rollup-plugin-svelte';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
