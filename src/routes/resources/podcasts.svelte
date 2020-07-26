1e60d347502da56997d442b9c5be337cee65a8d8
Pablo Hurtado <hurtado.pani@gmail.com>
2020-07-26 17:13:37 +0200

    Added podcasts page and "Lær Norsk Nå" entry (#8)
    Co-authored-by: Pablo Hurtado <pablo.hurtado@emea.adecco.net>

---
+++ src/routes/resources/podcasts.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Page from '../../components/ResourcePage.svelte';
+
+  const sites = [
+    {
+      name: 'Lær Norsk Nå!',
+      description: `<p>A podcast targeted at intermediate (B1-B2) learners, with transcriptions available. By Marius Stangeland.</p>`,
+      href: 'https://laernorsknaa.wixsite.com/laernorsknaa',
+      tags: {
+        transcriptions: 'yes',
+      },
+    },
+  ];
+</script>
+
+<Page items={sites} title="Podcasts"/>
